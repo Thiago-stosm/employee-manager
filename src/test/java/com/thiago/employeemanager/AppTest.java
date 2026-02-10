2b6839d4f25f1f5ed76eadf632bf4232c097f4f5
Thiago Santos <thiagosm2006@gmail.com>
2026-02-10 08:52:56 -0300

    feat: first commit, update local project

---
+++ src/test/java/com/thiago/employeemanager/AppTest.java
@@ -0,0 +1,14 @@
+package com.thiago.employeemanager;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void shouldRun() {
+        assertTrue(true);
+    }
+}
+
